b621166cad7329a72ab43d6e0cd90f5d493a1221
Tri <quachtri865@gmail.com>
2026-02-03 16:08:42 +0700

    init project

---
+++ src/main/java/com/group05/TC_LLM_Generator/TcLlmGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.group05.TC_LLM_Generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcLlmGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcLlmGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group05/TC_LLM_Generator/TcLlmGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group05.TC_LLM_Generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcLlmGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
